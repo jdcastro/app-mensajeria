ba7b84bb8f7eafc57ee5acdd0575339577b9118c
Johnny De Castro <j@jdcastro.co>
2022-10-05 18:51:23 -0500

    Ajustados los parámetros y corregido el código de la plantilla

--- app/inbox.py
+++ app/inbox.py
@@ -16,12 +16,14 @@ def getDB():
 @bp.route('/show')
 @login_required
 def show():
-    db = ?
+    db = get_db()
     messages = db.execute(
-        QUERY
+        'SELECT u.username AS username, m.subject AS subject, m.body AS body, m.created AS created'
+        ' FROM (select * from message where to_id='+str(g.user['id'])+') AS m JOIN User u ON  m.from_id = u.id'
+        ' ORDER BY created DESC'
     ).fetchall()
 
-    return render_template(TEMP, messages=messages)
+    return render_template('inbox/show.html', messages=messages)
 
 
 @bp.route('/send', methods=('GET', 'POST'))
@@ -29,29 +31,29 @@ def show():
 def send():
     if request.method == 'POST':        
         from_id = g.user['id']
-        to_username = ?
-        subject = ?
-        body = ?
+        to_username = request.form['to']
+        subject = request.form['subject']
+        body = request.form['body']
 
-        db = ?
+        db = get_db()
        
         if not to_username:
             flash('To field is required')
-            return render_template(TEMP)
+            return render_template('inbox/send.html')
         
-        if ?:
+        if not subject:
             flash('Subject field is required')
             return render_template('inbox/send.html')
         
-        if ?:
+        if not body:
             flash('Body field is required')
-            return render_template(TEMP)    
+            return render_template('inbox/send.html')
         
         error = None    
         userto = None 
         
         userto = db.execute(
-            QUERY, (to_username,)
+            'SELECT * FROM user WHERE username = ?', (to_username,)
         ).fetchone()
         
         if userto is None:
@@ -60,9 +62,10 @@ def send():
         if error is not None:
             flash(error)
         else:
-            db = ?
+            db = get_db()
             db.execute(
-                QUERY,
+                'INSERT INTO message (from_id, to_id, subject, body)'
+                ' VALUES (?, ?, ?, ?)',
                 (g.user['id'], userto['id'], subject, body)
             )
             db.commit()
